cd86f2c3b7bea6102d2026b978ba9eb608307125
minkyoung kim <minry20415@naver.com>
2022-11-07 21:27:59 +0900

    SpringBoot Mustache Bbs1 빌드

---
+++ src/test/java/com/mustache/bbs1/SpringbootMustache1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustache.bbs1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMustache1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
